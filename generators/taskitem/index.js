391612990efc8bf7d468791b737d29fc28c6db40
Mikael Krief <krief_mikael@hotmail.com>
2017-03-30 14:21:19 +0200

    - Update the template extension architecture - Add tfx-cli scripts into package.json

--- generators/taskitem/index.js
+++ generators/taskitem/index.js
@@ -180,15 +180,6 @@ function writeFiles() {
       this.destinationPath(taskFolder + '/icon.png')
    );
 
-   this.fs.copyTpl(
-      this.templatePath('gruntfile.js'),
-      this.destinationPath(extensionFolder + '/gruntfile.js'), tokens
-
-   );
-   this.fs.copyTpl(
-      this.templatePath('settings.tfx.json'),
-      this.destinationPath(extensionFolder + '/settings.tfx.json'), tokens
-   );
 
    this.fs.copyTpl(
       this.templatePath('vss-extension.json'),
@@ -259,9 +250,9 @@ function install() {
    // I don't want to see the output of this command
    this.spawnCommandSync('npm', ['install'], { stdio: ['pipe', 'pipe', process.stderr] });
 
-   this.log(`+ Running grunt for vsix generating`);
+   this.log(`+ Running npm run package for vsix generating`);
    process.chdir(that.destinationRoot() + "\\" + extensionFolder);
-   this.spawnCommandSync('grunt', ['exec:package'], { stdio: ['pipe', 'pipe', process.stderr] });
+   this.spawnCommandSync('npm', ['run','package'], { stdio: ['pipe', 'pipe', process.stderr] });
 
    done();
    this.log(`Done`);
