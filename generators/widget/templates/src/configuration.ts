56b449a7eb4d2929bd5add073d8f8ba1013fddda
Mikael Krief <krief_mikael@hotmail.com>
2017-04-05 18:22:20 +0200

    - Add widget Dashboard template + Tests - Fix folder name with extension ID - rename .npmrc to private.npmrc for fix CI Build

---
+++ generators/widget/templates/src/configuration.ts
@@ -0,0 +1,36 @@
+﻿VSS.require("TFS/Dashboards/WidgetHelpers", function (WidgetHelpers) {
+    WidgetHelpers.IncludeWidgetConfigurationStyles();
+    VSS.register("<%= WidgetId %>-Configuration", function () {
+        var $queryDropdown = $("#query-path");
+
+        return {
+            load: function (widgetSettings, widgetConfigurationContext) {
+                var settings = JSON.parse(widgetSettings.customSettings.data);
+                if (settings && settings.queryPath) {
+                    $queryDropdown.val(settings.queryPath);
+                }
+                $queryDropdown.on("change", function () {
+                    var customSettings = {
+                        data: JSON.stringify({
+                            queryPath: $queryDropdown.val()
+                        })
+                    };
+                    var eventName = WidgetHelpers.WidgetEvent.ConfigurationChange;
+                    var eventArgs = WidgetHelpers.WidgetEvent.Args(customSettings);
+                    widgetConfigurationContext.notify(eventName, eventArgs);
+                });
+
+                return WidgetHelpers.WidgetStatusHelper.Success();
+            },
+            onSave: function () {
+                var customSettings = {
+                    data: JSON.stringify({
+                        queryPath: $queryDropdown.val()
+                    })
+                };
+                return WidgetHelpers.WidgetConfigurationSave.Valid(customSettings);
+            }
+        }
+    });
+    VSS.notifyLoadSucceeded();
+});
