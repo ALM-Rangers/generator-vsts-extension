20b6551f6e7f6a901112019d92fa8c59041d431f
Mikael Krief <krief_mikael@hotmail.com>
2017-05-05 14:01:19 +0200

    add function run()

--- generators/taskitem/templates/task/task.ts
+++ generators/taskitem/templates/task/task.ts
@@ -1,15 +1,18 @@
 import tl = require('vsts-task-lib/task');
 //npm install vsts-task-lib
 
-try {
+// Get task parameters
+let variable1: string = tl.getPathInput('variable1', false, true);
+let variable2: string = tl.getInput('variable2', true);
 
-    // Get task parameters
-    let variable1: string = tl.getPathInput('variable1', false, true);
-    let variable2: string = tl.getInput('variable2', true);
 
-    //do your actions
 
+async function run() {
+    try {
+        //do your actions
+    } catch (err) {
+        tl.setResult(tl.TaskResult.Failed, err.message);
+    }
 }
-catch (err) {
-    tl.setResult(tl.TaskResult.Failed, err.message);
-}
+
+run();
